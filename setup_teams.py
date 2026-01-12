6641b1a5cd2943d7efbcf5797f0d2069aa564641
JosephCheung0327 <65944201+JosephCheung0327@users.noreply.github.com>
2026-01-12 00:46:27 -0500

    update

---
+++ setup_teams.py
@@ -0,0 +1,11 @@
+import requests
+import psycopg2
+
+DB_CONFIG = {
+    "dbname": "nhl_analytics",
+    "user": "postgres",
+    "password": "nhldb0112",
+    "host": "localhost",
+    "port": "5432"
+}
+
